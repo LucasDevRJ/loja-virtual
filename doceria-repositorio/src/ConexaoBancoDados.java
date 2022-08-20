46e8eeb8eb6c5e7738f9755db6eb18f5db592bf2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-20 14:26:14 -0300

    Removendo classe de treino

---
+++ doceria-repositorio/src/ConexaoBancoDados.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBancoDados {
+
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "djifjd", "dfd*");
+		
+		System.out.println("Funciona");
+		
+		conexao.close();
+	}
+}
