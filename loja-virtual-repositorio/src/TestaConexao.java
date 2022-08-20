911627a0b0a345ddf9a9caad5375e7ec56c73013
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-20 13:10:10 -0300

    Criando a conexão com o banco de dados

--- loja-virtual-repositorio/src/TestaConexao.java
+++ loja-virtual-repositorio/src/TestaConexao.java
@@ -1,5 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class TestaConexao {
 
-	
+	public static void main(String[] args) {
+		//Cria conexão com o banco de dados
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "wUjaspeF9u*");
+	}
 }
