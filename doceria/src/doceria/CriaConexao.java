d2ce3cc1c999224be13d2543d581dd9837153459
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 17:01:30 -0300

    Refatoração

--- doceria/src/doceria/CriaConexao.java
+++ doceria/src/doceria/CriaConexao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class CriaConexao {
 
-	public Connection CriaConexao() throws SQLException {
+	public Connection conecta() throws SQLException {
 		return DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "root", "root");
 	}
 }
--- doceria/src/doceria/Listagem.java
+++ doceria/src/doceria/Listagem.java
@@ -1,7 +1,6 @@
 package doceria;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,11 +9,14 @@ public class Listagem {
 
 	public static void main(String[] args) throws SQLException {
 		//Estabelecer a conexão com o banco
-		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "root", "root");
+		CriaConexao criaConexao = new CriaConexao();
+		Connection conexao = criaConexao.conecta();
 		
 		//Utilizar comandos do Banco de dados
 		Statement stm = conexao.createStatement();
 		boolean resultado = stm.execute("SELECT ID, NOME, INGREDIENTES FROM DOCE");
+		
+		System.out.println(resultado);
 		
 		//Pegar o resultado da lista
 		ResultSet itensLista = stm.getResultSet();
