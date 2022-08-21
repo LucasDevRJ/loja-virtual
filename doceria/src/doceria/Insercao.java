0442386356b28289ac3ddbf842f65e12a233c795
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 17:30:32 -0300

    Método de inserção finalizado

--- doceria/src/doceria/Insercao.java
+++ doceria/src/doceria/Insercao.java
@@ -1,6 +1,7 @@
 package doceria;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,6 +12,13 @@ public class Insercao {
 		Connection conexao = criaConexao.conecta();
 		
 		Statement stm = conexao.createStatement();
-		stm.execute("INSERT INTO doce (nome, ingredientes) VALUES ('Sequilho', 'Leite condesado, amido de milho, manteiga, leite ninho'", Statement.RETURN_GENERATED_KEYS);
+		stm.execute("INSERT INTO doce (nome, ingredientes) VALUES ('Sequilho', 'Leite condesado, amido de milho, manteiga, leite ninho')", Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while (rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O ID criado foi: " + id);
+		}
 	}
 }
