9126a8b5799b9a8a5fea11b5047b8792a0c61898
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 12:26:31 -0300

    Conectando com o banco de dados doceria

--- doceria/src/doceria/Conexao.java
+++ doceria/src/doceria/Conexao.java
@@ -1,5 +1,14 @@
 package doceria;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "root", "root");
+		
+		conexao.close();
+	}
 }
