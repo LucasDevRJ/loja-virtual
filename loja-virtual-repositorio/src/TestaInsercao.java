365df839344b051edf3f1cc658246c364868bea7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 17:19:53 -0300

    Adição do método de inserção de dados

--- loja-virtual-repositorio/src/TestaInsercao.java
+++ loja-virtual-repositorio/src/TestaInsercao.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaInsercao {
 
@@ -7,6 +9,15 @@ public class TestaInsercao {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 		
+		Statement stm = connection.createStatement();
+		boolean resultado = stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')", Statement.RETURN_GENERATED_KEYS);
+		System.out.println(resultado);
 		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while (rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: " + id);
+		}
 	}
 }
