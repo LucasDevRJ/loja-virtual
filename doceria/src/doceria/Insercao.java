c48969a03be0bc6945b1ee8fe68a250991b7b16e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 17:25:31 -0300

    Gerando dados no banco em Java

--- doceria/src/doceria/Insercao.java
+++ doceria/src/doceria/Insercao.java
@@ -2,11 +2,15 @@ package doceria;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Insercao {
 
 	public static void main(String[] args) throws SQLException {
 		CriaConexao criaConexao = new CriaConexao();
 		Connection conexao = criaConexao.conecta();
+		
+		Statement stm = conexao.createStatement();
+		stm.execute("INSERT INTO doce (nome, ingredientes) VALUES ('Sequilho', 'Leite condesado, amido de milho, manteiga, leite ninho'", Statement.RETURN_GENERATED_KEYS);
 	}
 }
