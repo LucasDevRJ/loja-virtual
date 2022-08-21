544d520f29879951f567aadd8cfbd4d229b89e6f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 13:05:29 -0300

    Exibindo os doces

--- doceria/src/doceria/Listagem.java
+++ doceria/src/doceria/Listagem.java
@@ -2,6 +2,7 @@ package doceria;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,7 +16,20 @@ public class Listagem {
 		Statement stm = conexao.createStatement();
 		boolean resultado = stm.execute("SELECT ID, NOME, INGREDIENTES FROM DOCE");
 		
-		System.out.println(resultado);
+		//Pegar o resultado da lista
+		ResultSet itensLista = stm.getResultSet();
+		
+		System.out.println("Cardápio");
+		System.out.println();
+		while (itensLista.next()) {
+			Integer id = itensLista.getInt("ID");
+			String nome = itensLista.getString("NOME");
+			String ingredientes = itensLista.getString("INGREDIENTES");
+			System.out.println("Código: " + id);
+			System.out.println("Nome: " + nome);
+			System.out.println("Ingredientes: " + ingredientes);
+			System.out.println();
+		}
 		
 		conexao.close();
 	}
