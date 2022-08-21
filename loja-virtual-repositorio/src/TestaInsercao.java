b23aeedeffabb358348db6b52cf187948b989a82
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 17:07:32 -0300

    Criando conexão na classe de inserção

--- loja-virtual-repositorio/src/TestaInsercao.java
+++ loja-virtual-repositorio/src/TestaInsercao.java
@@ -1,7 +1,12 @@
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TestaInsercao {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
 		
 	}
 }
