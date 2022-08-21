c47ccedbdb47af28403e54164f671141f3e00034
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 16:20:12 -0300

    Método finalizado para criar conexão

--- loja-virtual-repositorio/src/CriaConexao.java
+++ loja-virtual-repositorio/src/CriaConexao.java
@@ -1,5 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CriaConexao {
 
-	
+	//Método para criar a conexão com o Banco
+	public Connection recuperarConexao() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+	}
 }
