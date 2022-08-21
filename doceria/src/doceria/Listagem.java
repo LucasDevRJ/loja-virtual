19a54c9abc3241bc6eaa3cd53455cd6a4a1994f2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-21 12:51:41 -0300

    Adicionando comandos do banco de dados

--- doceria/src/doceria/Listagem.java
+++ doceria/src/doceria/Listagem.java
@@ -3,10 +3,20 @@ package doceria;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Listagem {
 
 	public static void main(String[] args) throws SQLException {
+		//Estabelecer a conexão com o banco
 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/doceria?useTimezone=true&serverTimezone=UTC", "root", "root");
+		
+		//Utilizar comandos do Banco de dados
+		Statement stm = conexao.createStatement();
+		boolean resultado = stm.execute("SELECT ID, NOME, INGREDIENTES FROM DOCE");
+		
+		System.out.println(resultado);
+		
+		conexao.close();
 	}
 }
