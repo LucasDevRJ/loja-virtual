1d60fb051df853de4198cb2d8f7d3344c4737e91
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 18:02:54 -0300

    Exclusão dos dados repetidos

--- doceria/src/doceria/Remocao.java
+++ doceria/src/doceria/Remocao.java
@@ -2,11 +2,15 @@ package doceria;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Remocao {
 
 	public static void main(String[] args) throws SQLException {
 		CriaConexao criaConexao = new CriaConexao();
 		Connection conexao = criaConexao.conecta();
+		
+		Statement stm = conexao.createStatement();
+		stm.execute("DELETE FROM DOCE WHERE ID > 2");
 	}
 }
