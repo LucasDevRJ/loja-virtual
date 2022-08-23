a220c39a8db6aa0f20f881cab911613f44e77af0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 17:57:34 -0300

    Criando conexão Java com o banco

--- doceria/src/doceria/Remocao.java
+++ doceria/src/doceria/Remocao.java
@@ -1,5 +1,12 @@
 package doceria;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Remocao {
 
+	public static void main(String[] args) throws SQLException {
+		CriaConexao criaConexao = new CriaConexao();
+		Connection conexao = criaConexao.conecta();
+	}
 }
