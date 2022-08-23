4f113fcd9d553cc8f4a6b2f14d0f1ae8ebc16322
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 19:24:47 -0300

    Inserção de dados com parametros

--- loja-virtual-repositorio/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositorio/src/TestaInsercaoComParametro.java
@@ -6,11 +6,14 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
+		String nome = "Mouse";
+		String descricao = "Mouse sem fio";
+		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 		
 		Statement stm = connection.createStatement();
-		boolean resultado = stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio')", Statement.RETURN_GENERATED_KEYS);
+		boolean resultado = stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('" + nome + "', '" + descricao + "')", Statement.RETURN_GENERATED_KEYS);
 		System.out.println(resultado);
 		
 		ResultSet rst = stm.getGeneratedKeys();
