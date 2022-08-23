8423dff0490d07099394276e48d3e15915501342
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 17:45:06 -0300

    Adição da interface para modificar o banco de dados

--- loja-virtual-repositorio/src/TestaRemocao.java
+++ loja-virtual-repositorio/src/TestaRemocao.java
@@ -1,10 +1,13 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaRemocao {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
 	}
 }
