4d0c4d886b661054d90d6d84bfd1d595b04d8c24
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 18:26:20 -0300

    Criando conexão com banco de dados

--- loja-virtual-repositorio/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositorio/src/TestaInsercaoComParametro.java
@@ -1,4 +1,11 @@
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TestaInsercaoComParametro {
 
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+	}
 }
