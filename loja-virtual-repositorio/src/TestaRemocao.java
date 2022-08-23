5db26af22a2107a92ddf5aca3f4541b124657173
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-23 17:43:51 -0300

    Adição da classe para recuperar conexão

--- loja-virtual-repositorio/src/TestaRemocao.java
+++ loja-virtual-repositorio/src/TestaRemocao.java
@@ -1,8 +1,10 @@
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TestaRemocao {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		
+		Connection connection = factory.recuperarConexao();
 	}
 }
