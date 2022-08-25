faa15ebd80a28f627db6328fd71631e4732eff8d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-24 21:22:32 -0300

    Copiando códigos da classe Insercao para InsercaoParametro

--- doceria/src/doceria/InsercaoParametro.java
+++ doceria/src/doceria/InsercaoParametro.java
@@ -1,5 +1,24 @@
 package doceria;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class InsercaoParametro {
 
+	public static void main(String[] args) throws SQLException {
+		CriaConexao criaConexao = new CriaConexao();
+		Connection conexao = criaConexao.conecta();
+		
+		Statement stm = conexao.createStatement();
+		stm.execute("INSERT INTO doce (nome, ingredientes) VALUES ('Sequilho', 'Leite condesado, amido de milho, manteiga, leite ninho')", Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while (rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O ID criado foi: " + id);
+		}
+	}
 }
