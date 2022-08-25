e53c6f70d9be1f44f83b747f62a8c1e0aa68ba06
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-24 21:19:22 -0300

    Criação de classe para testar parametro

---
+++ doceria/src/doceria/InsercaoParametro.java
@@ -0,0 +1,5 @@
+package doceria;
+
+public class InsercaoParametro {
+
+}
--- loja-virtual-repositorio/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositorio/src/TestaInsercaoComParametro.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,8 +13,14 @@ public class TestaInsercaoComParametro {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 		
-		Statement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)");
-		stm.execute(Statement.RETURN_GENERATED_KEYS);
+		//Para permitir os parametros e transformar tudo em String, dando mais segurança para o sistema
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+		
+		//Setar os atributos do banco, pegando como referência o ? do Statement
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
 		
 		
 		ResultSet rst = stm.getGeneratedKeys();
