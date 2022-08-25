38a2d02556fd0424698cb78dbba6d30e47dc2a85
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-25 20:08:27 -0300

    Método de adicionar doces concluído

--- doceria/src/doceria/InsercaoParametro.java
+++ doceria/src/doceria/InsercaoParametro.java
@@ -9,22 +9,25 @@ import java.sql.Statement;
 public class InsercaoParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse";
-		String descricao = "Mouse sem fio";
-		
 		CriaConexao criaConexao = new CriaConexao();
 		Connection conexao = criaConexao.conecta();
+		conexao.setAutoCommit(false); //controlar as transações
 		
 		//Gerencia os dados inseridos
 		PreparedStatement stm = conexao.prepareStatement("INSERT INTO doce (nome, ingredientes) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-		adicionarDoce("", "", stm);
+		adicionarDoce("Pudim de leite", "Gelatina, água, leite condesado, creme de leite, baunilha e leite", stm);
+		adicionarDoce("Danoninho", "Leite condesado, creme de leite e morango", stm);
 	}
 
 	public static void adicionarDoce(String nome, String ingredientes, PreparedStatement stm) throws SQLException {
 		//Settar os atributos (? ?)
 		stm.setString(1, nome);
 		stm.setString(2, ingredientes);
-				
+			
+		if (stm.equals("Danoninho")) {
+			throw new RuntimeException("Não foi possivel adicionar o doce!!!");
+		}
+		
 		stm.execute();
 				
 		ResultSet rst = stm.getGeneratedKeys();
@@ -33,5 +36,6 @@ public class InsercaoParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O ID criado foi: " + id);
 		}
+		rst.close();
 	}
 }
