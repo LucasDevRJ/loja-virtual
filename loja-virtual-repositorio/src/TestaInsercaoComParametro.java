5c59b6ad3aca67c8e8a72bdb3a0de513253b4352
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-27 11:15:14 -0300

    Controle melhor das transações

--- loja-virtual-repositorio/src/TestaInsercaoComParametro.java
+++ loja-virtual-repositorio/src/TestaInsercaoComParametro.java
@@ -11,26 +11,35 @@ public class TestaInsercaoComParametro {
 		Connection connection = factory.recuperarConexao();
 		connection.setAutoCommit(false); //para controlar a transação
 		
-		//Para permitir os parametros e transformar tudo em String, dando mais segurança para o sistema
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-		
-		//Setar os atributos do banco, pegando como referência o ? do Statement
-		adicionarVariavel("SmartTV", "45 polegadas", stm);
-		adicionarVariavel("Radio", "Radio de bateria", stm);
-		
-		connection.commit();
-		
-		stm.close();
-		connection.close();
-	}
+		//Ter o controle melhor das transações
+		try {
+			//Para permitir os parametros e transformar tudo em String, dando mais segurança para o sistema
+			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+			
+			//Setar os atributos do banco, pegando como referência o ? do Statement
+			adicionarVariavel("SmartTV", "45 polegadas", stm);
+			adicionarVariavel("Radio", "Radio de bateria", stm);
+			
+			//Evitar conflitos de erro de transação
+			connection.commit();
+			
+			stm.close();
+			connection.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			connection.rollback();
+		}
+	} 
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-//		
-//		if (nome.equals("Radio")) {
-//			throw new RuntimeException("Não foi possivel adicionar o produto!!!");
-//		}
+		
+		if (nome.equals("Radio")) {
+			throw new RuntimeException("Não foi possivel adicionar o produto!!!");
+		}
 		
 		stm.execute();
 		
