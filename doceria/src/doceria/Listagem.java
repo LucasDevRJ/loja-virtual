d9cb87e82486c6ed93ec69455358934f4dff1e7b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-28 15:31:45 -0300

    Adição de jars externos

--- doceria/src/doceria/Listagem.java
+++ doceria/src/doceria/Listagem.java
@@ -8,33 +8,34 @@ import java.sql.SQLException;
 public class Listagem {
 
 	public static void main(String[] args) throws SQLException {
-		//Estabelecer a conexão com o banco
+		// Estabelecer a conexão com o banco
 		CriaConexao criaConexao = new CriaConexao();
-		Connection conexao = criaConexao.conecta();
-		
-		//Utilizar comandos do Banco de dados, gerenciando-os
-		PreparedStatement stm = conexao.prepareStatement("SELECT ID, NOME, INGREDIENTES FROM DOCE");
-		
-		//Operação lógica para saber se esta funcionando
-		boolean resultado = stm.execute();
-		
-		System.out.println(resultado);
-		
-		//Pegar o resultado da lista
-		ResultSet itensLista = stm.getResultSet();
-		
-		System.out.println("Cardápio");
-		System.out.println();
-		while (itensLista.next()) {
-			Integer id = itensLista.getInt("ID");
-			String nome = itensLista.getString("NOME");
-			String ingredientes = itensLista.getString("INGREDIENTES");
-			System.out.println("Código: " + id);
-			System.out.println("Nome: " + nome);
-			System.out.println("Ingredientes: " + ingredientes);
-			System.out.println();
+		try (Connection conexao = criaConexao.conecta()) {
+
+			// Utilizar comandos do Banco de dados, gerenciando-os
+			try (PreparedStatement stm = conexao.prepareStatement("SELECT ID, NOME, INGREDIENTES FROM DOCE")) {
+
+				// Operação lógica para saber se esta funcionando
+				boolean resultado = stm.execute();
+
+				System.out.println(resultado);
+
+				// Pegar o resultado da lista
+				try (ResultSet itensLista = stm.getResultSet()) {
+
+					System.out.println("Cardápio");
+					System.out.println();
+					while (itensLista.next()) {
+						Integer id = itensLista.getInt("ID");
+						String nome = itensLista.getString("NOME");
+						String ingredientes = itensLista.getString("INGREDIENTES");
+						System.out.println("Código: " + id);
+						System.out.println("Nome: " + nome);
+						System.out.println("Ingredientes: " + ingredientes);
+						System.out.println();
+					}
+				}
+			}
 		}
-		
-		conexao.close();
 	}
 }
