184ad6cac82c438394f2fe873de6ac93ccee0098
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-28 17:54:15 -0300

    Limitando número de conexões no servidor no projeto doceria

--- doceria/src/doceria/PoolConexoes.java
+++ doceria/src/doceria/PoolConexoes.java
@@ -1,9 +1,15 @@
 package doceria;
 
+import java.sql.SQLException;
+
 public class PoolConexoes {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		CriaConexao conexao = new CriaConexao();
 		
+		for (int i = 0; i <= 10; i++) {
+			conexao.conecta();
+			System.out.println("Conexão de número: " + i);
+		}
 	}
 }
