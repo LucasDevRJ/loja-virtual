98fbe68d9f509dffd5c301201ece5ac6799721fa
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-28 15:45:49 -0300

    Ligação da classe ConnectionFactory com o banco

--- loja-virtual-repositorio/src/ConnectionFactory.java
+++ loja-virtual-repositorio/src/ConnectionFactory.java
@@ -2,19 +2,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
+	
+	public DataSource dataSource;
 	
 	public ConnectionFactory() {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
 		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
 		comboPooledDataSource.setUser("root");
 		comboPooledDataSource.setPassword("root");
+		
+		this.dataSource = comboPooledDataSource;
 	}
 
 	//Método para criar a conexão com o Banco
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		return this.dataSource.getConnection();
 	}
 }
