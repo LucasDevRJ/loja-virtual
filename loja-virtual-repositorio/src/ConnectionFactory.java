a587c345acd154ad0df19a0c943942fb013ceac2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-28 15:38:41 -0300

    Criação do construtor ConnectionFactory

--- loja-virtual-repositorio/src/ConnectionFactory.java
+++ loja-virtual-repositorio/src/ConnectionFactory.java
@@ -2,7 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+	}
 
 	//Método para criar a conexão com o Banco
 	public Connection recuperarConexao() throws SQLException {
