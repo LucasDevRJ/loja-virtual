8ddaf248f8c54fc66dba6fa02e2a5bfd020fe60a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-29 21:48:17 -0300

    Criação da classe Doce e seus atributos

---
+++ doceria/src/doceria/Doce.java
@@ -0,0 +1,20 @@
+package doceria;
+
+public class Doce {
+
+	private Integer id;
+	private String nome;
+	private String ingredientes;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getIngredientes() {
+		return ingredientes;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
