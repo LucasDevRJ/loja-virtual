d37ab08fc20b217ed44e56b01d8b4f60f6094c21
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-30 09:24:07 -0300

    Classe DoceDAO completada

--- doceria/src/doceria/TestaInsercaoDoce.java
+++ doceria/src/doceria/TestaInsercaoDoce.java
@@ -2,4 +2,9 @@ package doceria;
 
 public class TestaInsercaoDoce {
 
+	public static void main(String[] args) {
+		Doce doce = new Doce("Muffin de chocolate", "Chocolate amargo, leite quente, manteiga, ovo.");
+		
+		
+	}
 }
