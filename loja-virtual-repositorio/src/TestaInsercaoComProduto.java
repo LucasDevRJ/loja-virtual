c5458531abeb00ea140da2f6f4ae7f1512855cae
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-29 21:25:02 -0300

    Adição do try catch na classe para testar o produto

--- loja-virtual-repositorio/src/TestaInsercaoComProduto.java
+++ loja-virtual-repositorio/src/TestaInsercaoComProduto.java
@@ -1,8 +1,15 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaInsercaoComProduto {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		Produto produto = new Produto("Cômoda", "Cômoda vertical");
+		
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+		}
 	}
 }
