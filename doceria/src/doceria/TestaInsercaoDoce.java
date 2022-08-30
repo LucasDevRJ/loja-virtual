75ccf0c1cd055219a7b9f98b244c145883891ea8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-30 09:28:08 -0300

    Refatoração na classe Doce

--- doceria/src/doceria/TestaInsercaoDoce.java
+++ doceria/src/doceria/TestaInsercaoDoce.java
@@ -1,10 +1,20 @@
 package doceria;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import doceria.dao.DoceDAO;
+
 public class TestaInsercaoDoce {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		Doce doce = new Doce("Muffin de chocolate", "Chocolate amargo, leite quente, manteiga, ovo.");
 		
-		
+		try (Connection conexao = new CriaConexao().conecta()) {
+			String sql = "INSERT INTO DOCE (NOME, INGREDIENTES) VALUES (?, ?)";
+			
+			DoceDAO doceDao = new DoceDAO(conexao);
+			doceDao.salvar(doce);
+		}
 	}
 }
