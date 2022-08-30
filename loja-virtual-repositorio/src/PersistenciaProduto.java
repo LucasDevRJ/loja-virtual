1c2e0a37fb67bfa24561b67edc4a17fced15057a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-30 08:17:00 -0300

    Refatoração no código

--- loja-virtual-repositorio/src/PersistenciaProduto.java
+++ loja-virtual-repositorio/src/PersistenciaProduto.java
@@ -1,11 +1,33 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.com.alura.jdbc.modelo.Produto;
 
 public class PersistenciaProduto {
-	
+
 	private Connection connection;
-	
+
 	public PersistenciaProduto(Connection connection) {
 		this.connection = connection;
 	}
 
+	public void salvarProduto(Produto produto) throws SQLException {
+		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			pstm.setString(1, produto.getNome());
+			pstm.setString(2, produto.getDescricao());
+
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getGeneratedKeys()) {
+				while (rst.next()) {
+					produto.setId(rst.getInt(1));
+				}
+			}
+		}
+	}
 }
--- loja-virtual-repositorio/src/TestaInsercaoComProduto.java
+++ loja-virtual-repositorio/src/TestaInsercaoComProduto.java
@@ -14,18 +14,7 @@ public class TestaInsercaoComProduto {
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
 			
-			try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(2, comoda.getDescricao());
-				
-				pstm.execute();
-				
-				try (ResultSet rst = pstm.getGeneratedKeys()) {
-					while (rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-			}
+			new PersistenciaProduto(connection).salvarProduto(comoda);
 		}
 		System.out.println(comoda);
 	}
