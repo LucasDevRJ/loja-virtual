643e88844141ca28de464689d410c5a82d9c2660
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-29 21:51:13 -0300

    Criação do construtor da classe Doce

--- doceria/src/doceria/Doce.java
+++ doceria/src/doceria/Doce.java
@@ -6,6 +6,11 @@ public class Doce {
 	private String nome;
 	private String ingredientes;
 	
+	public Doce(String nome, String ingredientes) {
+		this.nome = nome;
+		this.ingredientes = ingredientes;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
---
+++ doceria/src/doceria/InsercaoDoce.java
@@ -0,0 +1,8 @@
+package doceria;
+
+public class InsercaoDoce {
+
+	public static void main(String[] args) {
+		
+	}
+}
