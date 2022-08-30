ffc62cfd78942011551bbf9d29d5fc74ed491232
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-29 22:00:40 -0300

    Classe para testar doce finalizada

--- doceria/src/doceria/InsercaoDoce.java
+++ doceria/src/doceria/InsercaoDoce.java
@@ -1,8 +1,31 @@
 package doceria;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class InsercaoDoce {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Doce doce = new Doce("Mousse de chocolate", "Chocolate ao leite, creme de leite e claras de ovo");
 		
+		try (Connection conexao = new CriaConexao().conecta()) {
+			String sql = "INSERT INTO DOCE (NOME, INGREDIENTES) VALUES (?, ?)";
+			
+			try (PreparedStatement ps = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+				ps.setString(1, doce.getNome());
+				ps.setString(2, doce.getIngredientes());
+				
+				ps.execute();
+				
+				try (ResultSet rs = ps.getGeneratedKeys()) {
+					while (rs.next()) {
+						doce.setId(rs.getInt(1));
+					}
+				}
+			}
+		}
 	}
 }
