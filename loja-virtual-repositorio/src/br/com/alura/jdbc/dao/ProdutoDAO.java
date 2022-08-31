3c6eaf76a634c15bdc88cb37e54b71a105bccc54
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-31 19:18:53 -0300

    Modificações na classe para chamar a lista

--- loja-virtual-repositorio/src/br/com/alura/jdbc/dao/ProdutoDAO.java
+++ loja-virtual-repositorio/src/br/com/alura/jdbc/dao/ProdutoDAO.java
@@ -50,5 +50,6 @@ public class ProdutoDAO {
 				}
 			}
 		}
+		return produtos;
 	}
 }
--- loja-virtual-repositorio/src/br/com/alura/jdbc/produto/TestaInsercaoEListagemComProduto.java
+++ loja-virtual-repositorio/src/br/com/alura/jdbc/produto/TestaInsercaoEListagemComProduto.java
@@ -4,11 +4,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 		Produto comoda = new Produto("Cômoda", "Cômoda vertical");
@@ -18,6 +19,8 @@ public class TestaInsercaoComProduto {
 			
 			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
 			produtoDAO.salvar(comoda);
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
 		System.out.println(comoda);
 	}
