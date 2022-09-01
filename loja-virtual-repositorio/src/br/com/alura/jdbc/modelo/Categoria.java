b319cafa6c8f6561d8ca9a231443144fbe2888b1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-01 18:29:37 -0300

    Criação de atributos na classe

--- loja-virtual-repositorio/src/br/com/alura/jdbc/modelo/Categoria.java
+++ loja-virtual-repositorio/src/br/com/alura/jdbc/modelo/Categoria.java
@@ -2,6 +2,12 @@ package br.com.alura.jdbc.modelo;
 
 public class Categoria {
 	
+	private Integer id;
+	private String nome;
 	
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
 
 }
